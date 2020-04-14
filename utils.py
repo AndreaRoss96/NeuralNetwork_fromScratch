3e1eddb493d904b80fa36b18eb3047768b0e4dd1
filorazi <orazifilippo@gmail.com>
2020-04-14 16:40:21 +0200

    utils

--- utils.py
+++ utils.py
@@ -1,32 +1,28 @@
 import numpy as np
 import pandas as pd
 
-# Read csv file for (X, y, n_classes) data
-def read_csv(csv_filename, target_name="y", normalize=False):
-    df = pd.read_csv(csv_filename, delimiter=",", dtype={target_name: str})
-    if list(df.columns.values).count(target_name) != 1: # check target
-        raise Exception("Need exactly 1 count of '{}' in {}".format(target_name, csv_filename))
-    target2idx = {target: idx for idx, target in enumerate(sorted(list(set(df[target_name].values))))}
-    X = df.drop([target_name], axis=1).values
-    y = np.vectorize(lambda x: target2idx[x])(df[target_name].values)
-    n_classes = len(target2idx.keys())
-    if X.shape[0] != y.shape[0]:
-        raise Exception("X.shape = {} and y.shape = {} are inconsistent!".format(X.shape, y.shape))
-    if normalize:
+# Reads the csv files and normalizes it if needed
+def read_csv(csv_filename, target="y", norm=False):
+    df = pd.read_csv(csv_filename, delimiter=",", dtype={target: str})
+    target2 = {target: i for i, target in enumerate(sorted(list(set(df[target].values))))}
+    X = df.drop([target], axis=1).values
+    y = np.vectorize(lambda x: target2[x])(df[target].values)
+    n_classes = len(target2.keys())
+    if norm:
         X = (X - X.mean(axis=0)) / X.std(axis=0)
     return X, y, n_classes
 
-# Randomly permute [0,N] and extract indices for each fold
+# folds the data and rturns the index of each fold
 def crossval_folds(N, n_folds, seed=1):
     np.random.seed(seed)
-    idx_all_permute = np.random.permutation(N)
-    N_fold = int(N/n_folds)
-    idx_folds = []
+    permuted_index = np.random.permutation(N)
+    fold_size = int(N/n_folds)
+    fold_index = []
     for i in range(n_folds):
-        start = i*N_fold
-        end = min([(i+1)*N_fold, N])
-        idx_folds.append(idx_all_permute[start:end])
-    return idx_folds
+        start = i*fold_size
+        end = min([(i+1)*fold_size, N])
+        fold_index.append(permuted_index[start:end])
+    return fold_index
 
 def dotprod(self, a, b):
     return sum([a_ * b_ for (a_, b_) in zip(a, b)])
